715fc9495707763a6158f2e50d66874fb8594a6d
milad.mmd <milad.mmd68@gmail.com>
2022-09-07 13:19:18 +0430

    complete database migration

--- migrations.php
+++ migrations.php
@@ -1,1 +1,32 @@
 <?php
+
+
+require_once __DIR__ . '/vendor/autoload.php';
+use app\core\Application;
+use app\controllers\SiteController;
+use app\controllers\AuthController;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
+
+
+$config = [
+    'db' => [
+        'dsn' => $_ENV['DB_DSN'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD'],
+    ]
+];
+
+
+
+$app = new Application(__DIR__,$config);
+
+
+
+
+
+
+$app->db->applyMigration();
